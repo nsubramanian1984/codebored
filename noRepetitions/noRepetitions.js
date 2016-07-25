1b9c2a53255df2bce9616be2e070218ad49fc058
Subramanian Narasimhan <subramanian.narasimhan@gmail.com>
2016-07-25 22:25:31 +0800

    no repetitions
    Made use of the existing compress algorithm.

---
+++ noRepetitions/noRepetitions.js
@@ -0,0 +1,48 @@
+(function() {
+
+    'use strict';
+
+    function compress(str) {
+        var result = '';
+        var regex = /^[a-zA-Z\,\?\.\s]+$/;
+        var result = '';
+
+
+        while(regex.test(str)) {
+            var res = str.match(/^[a-zA-Z\,\?\.\s]/);
+
+            if(res[0]) {
+
+                if(res[0] === '?')
+                    res[0] = '.';
+                else if(res[0] === ',')
+                    res[0] = '\,';
+
+                var anotherregex = '^(' + res[0] + ')+';
+
+                str = str.replace(new RegExp(anotherregex), function(a, b) {
+
+                    result += b;
+                    return '';
+                });
+            }
+            else
+                return;
+        }
+
+        return result;
+    }
+
+    //Sample code to read in test cases:
+    let fs  = require("fs");
+
+    fs.readFileSync(process.argv[2]).toString().split('\n').forEach(function (line) {
+        if (line !== "") {
+            //console.log(line);
+            console.log(compress(line));
+        }
+    });
+
+
+
+})();
