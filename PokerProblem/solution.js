2e7e6330738af9d96e64ea411b055eb8fad45976
Subramanian <subramanian.narasimhan@gmail.com>
2018-04-02 16:58:13 +0800

    Added few test cases for testing high card, four of a kind, two pairs, and a pair.
    Some modifications to code as well.

--- PokerProblem/solution.js
+++ PokerProblem/solution.js
@@ -17,6 +17,19 @@
                 return '0' + input;
         }
 
+        function rankLetter(value) {
+            if (value == 14)
+                return 'A';
+            else if (value == 13)
+                return 'K';
+            else if (value == 12)
+                return 'Q';
+            else if (value == 11)
+                return 'J';
+            else
+                return '' + value;
+        }
+
         function suit(input) {
             if (input === 'S')
                 return '1';
@@ -28,7 +41,7 @@
                 return '4';
         }
 
-        function count(value, list) {
+        function numberOfOccurrences(value, list) {
             let occur = 0;
 
             list.forEach(elem => {
@@ -39,14 +52,13 @@
             return occur;
         }
 
-        function determineThreeOfaKind(tuple, list) {
-
+        function determineTheKind(tuple, list, count) {
             let highRank = -1;
 
             for (let elem of tuple) {
-                let occurrences = count(elem, list);
+                let occurrences = numberOfOccurrences(elem, list);
 
-                if (occurrences === 3) {
+                if (occurrences === count) {
 
                     highRank = elem;
                     break;
@@ -54,29 +66,25 @@
             }
 
             return highRank;
+        }
 
+        function determineFourOfaKind(tuple, list) {
+            return determineTheKind(tuple, list, 4);
+        }
+
+        function determineThreeOfaKind(tuple, list) {
+            return determineTheKind(tuple, list, 3);
         }
 
         function determineAPair(tuple, list) {
-            let highRank = -1;
-
-            for (let elem of tuple) {
-                let occurrences = count(elem, list);
-
-                if (occurrences === 2) {
-                    highRank = elem;
-                    break;
-                }
-            }
-
-            return highRank;
+            return determineTheKind(tuple, list, 2);
         }
 
         function determineTwoPairsHighRank(tuple, list) {
             let highRank = -1;
 
             for (let elem of tuple) {
-                let occurrences = count(elem, list);
+                let occurrences = numberOfOccurrences(elem, list);
 
                 if (occurrences === 2) {
                     if (highRank === -1)
@@ -89,11 +97,44 @@
             return highRank;
         }
 
+        function determineHighValuefromList(list) {
+
+            //Assume that the list is sorted.
+            return list !== null ? list[list.length - 1] : "-1";
+
+        }
+
+        function determineHighCardFromTwoHands(left, right) {
+            for (let index = left.length - 1; index >= 0; index--) {
+                if (left[index] === right[index])
+                    continue;
+
+                return parseInt(left[index]) > parseInt(right[index]) ? "left" : "right";
+            }
+        }
+
         function determineWinner(left, right) {
             if (left.score < right.score)
                 return "left";
             else if (left.score === right.score) {
-                if (left.highrank === right.highrank)
+                if (left.highrank === right.highrank && (left.score === 9 || left.score === 10)) {
+
+                    // In case, a pair with same rank
+                    // 4D 6S 9H QH QC                   3D 6D 7H QD QS
+                    // Pair of Queens                   Pair of Queens
+                    // Highest card Nine                Highest card Seven
+
+                    // Need to look for the next highest rank among the cards.
+
+                    //5D 8C 9S JS AC                    2C 5C 7D 8S AH
+                    //Highest card Ace                  Highest card Queen
+
+                    if (left.rest !== null) console.log(left.rest);
+                    if (right.rest !== null) console.log(right.rest);
+
+                    return determineHighCardFromTwoHands(left.rest, right.rest);
+                }
+                else if (left.highrank === right.highrank)
                     return "none";
                 else
                     return parseInt(left.highrank) > parseInt(right.highrank) ? "left" : "right";
@@ -102,6 +143,28 @@
                 return "right";
         }
 
+        function handsDescription(value) {
+            if (value === 10)
+                return "High Card";
+            else if (value === 9)
+                return "One Pair";
+            else if (value === 8)
+                return "Two Pairs";
+            else if (value === 7)
+                return "Three of a Kind";
+            else if (value === 6)
+                return "Straight";
+            else if (value === 5)
+                return "Flush";
+            else if (value === 4)
+                return "Full House";
+            else if (value === 3)
+                return "Four of a Kind";
+            else if (value === 2)
+                return "Straight Flush";
+            else
+                return "Royal Flush"
+        }
 
         let leftHandScore, rightHandScore;
 
@@ -125,13 +188,26 @@
 
             if (rankTuple.length === 2) {
                 // might be a full house or four of a kind
-                // there is a chance for three of a kind and a pair.
-                // e.g: 8C 8S KC KH 8D
-
-                if (i === 0)
-                    leftHandScore = 4;
-                else
-                    rightHandScore = 4;
+                if (i === 0) {
+                    let highRank = determineFourOfaKind(rankTuple, result);
+                    if (highRank !== -1)
+                        leftHandScore = { "highrank": highRank, "score": 3 };
+                    else {
+                        highRank = determineThreeOfaKind(rankTuple, result);
+                        if (highRank !== -1)
+                            leftHandScore = { "highrank": highRank, "score": 4 };
+                    }
+                }
+                else {
+                    let highRank = determineFourOfaKind(rankTuple, result);
+                    if (highRank !== -1)
+                        rightHandScore = { "highrank": highRank, "score": 3 };
+                    else {
+                        highRank = determineThreeOfaKind(rankTuple, result);
+                        if (highRank !== -1)
+                            rightHandScore = { "highrank": highRank, "score": 4 };
+                    }
+                }
 
             } else if (rankTuple.length === 3) {
                 // might be a three of a kind or two pairs
@@ -154,25 +230,38 @@
                 }
 
             } else if (rankTuple.length === 4) {
+                let hiRank = determineAPair(rankTuple, result);
+
+                let nextHighRanks = rankTuple.filter((elem) => {
+                    if (elem != hiRank)
+                        return elem;
+                });
 
                 if (i === 0)
-                    leftHandScore = { "highrank": determineAPair(rankTuple, result), "score": 9 };
+                    leftHandScore = { "highrank": hiRank, "score": 9, "rest": nextHighRanks };
                 else
-                    rightHandScore = { "highrank": determineAPair(rankTuple, result), "score": 9 };
+                    rightHandScore = { "highrank": hiRank, "score": 9, "rest": nextHighRanks };
 
             } else {
                 // dig thru the cards to see what we have got
                 // Royal flush, straight flush, flush, straight, high card
 
+                let hiRank = determineHighValuefromList(rankTuple);
+
+                let nextHighRanks = rankTuple.filter((elem) => {
+                    if (elem != hiRank)
+                        return elem;
+                });
+
                 if (i === 0)
-                    leftHandScore = 10;
+                    leftHandScore = { "highrank": hiRank, "score": 10, "rest": nextHighRanks };
                 else
-                    rightHandScore = 10;
+                    rightHandScore = { "highrank": hiRank, "score": 10, "rest": nextHighRanks };
             }
         }
 
-        //console.log("Left: " + leftHandScore.highrank + " " + leftHandScore.score);
-        //console.log("Right: " + rightHandScore.highrank + " " + rightHandScore.score);
+        console.log("Left: " + rankLetter(leftHandScore.highrank) + " " + handsDescription(leftHandScore.score));
+        console.log("Right: " + rankLetter(rightHandScore.highrank) + " " + handsDescription(rightHandScore.score));
 
         console.log(determineWinner(leftHandScore, rightHandScore));
     }
